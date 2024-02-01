216f493cb06e375c338b91c2e68aa1c4d14106a1
Omor Faruk Mahfuj <mypcfaruk+github@gmail.com>
2024-02-01 17:53:59 +0600

    string added

---
+++ string.js
@@ -0,0 +1,10 @@
+const country = 'Bangladesh';
+const division = 'Dhaka';
+const district = 'Dhaka';
+const thana = new String('Polton');
+
+console.log(thana);
+console.log(thana.length);
+console.log(country[0]);
+
+// String Is Immutable
