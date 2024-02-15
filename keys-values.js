5ffc0b1b103fcc3eebe5ba511ff683589824022e
Omor Faruk Mahfuj <mypcfaruk+github@gmail.com>
2024-02-15 13:02:21 +0600

    keys-values added

---
+++ keys-values.js
@@ -0,0 +1,14 @@
+const pen = {
+  brand: 'matador',
+  inkColor: 'black',
+  bodyColor: 'pink',
+  type: 'ball pen',
+  price: 10
+}
+
+const keys = Object.keys(pen);
+console.log(keys);
+
+const values = Object.values(pen);
+
+console.log(values);
