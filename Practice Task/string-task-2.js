2aa543ccebb9ad65312a4ba9df63bfa55b00e1d2
Omor Faruk Mahfuj <mypcfaruk+github@gmail.com>
2024-02-15 20:28:32 +0600

    string-task-2 added

--- Practice Task/string-task-2.js
+++ Practice Task/string-task-2.js
@@ -1,7 +1,7 @@
 const myString = "JavaScript Is Amazing!";
 let count = 0;
 for (let i = 0; i < myString.length; i++) {
-  if (myString[i] === 'a') {
+  if (myString[i] === 'a' || myString[i] === 'A') {
     count++;
   }
 }
