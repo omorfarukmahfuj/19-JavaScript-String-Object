513a62fa12443bebbdf090beefee6b44a766fb30
Omor Faruk Mahfuj <mypcfaruk+github@gmail.com>
2024-02-15 11:16:23 +0600

    string updated

--- string.js
+++ string.js
@@ -1,8 +1,9 @@
 const country = 'Bangladesh';
 const division = 'Dhaka';
-const district = 'Dhaka';
+const district = `Dhaka`;
 const thana = new String('Polton');
 
+console.log(typeof district);
 console.log(thana);
 console.log(thana.length);
 console.log(country[0]);
